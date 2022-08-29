51f2ba8f58ff6fea02c10a797a0f5ee29fd6acfe
Quinn <44106439+quinnledingham@users.noreply.github.com>
2022-08-29 11:16:54 -0600

    Fixing sound loading

--- project/code/audio_visualizer.cpp
+++ project/code/audio_visualizer.cpp
@@ -61,7 +61,7 @@ MakeAssetFile(platform_work_queue *Queue, assets *Assets)
     //
     // Sounds
     //
-    BuilderAddSound(&BuilderAssets, "bornintheusa.wav", Asset_Song);
+    BuilderAddSound(&BuilderAssets, "bonk.wav", Asset_Song);
     
     //
     // Fonts
@@ -92,13 +92,11 @@ pair_int_string IDs[] = {
 void
 UpdateRender(platform *p)
 {
-    app_state *AppState = (app_state*)p->Memory.PermanentStorage;
+    app_state *AppState = MemStart(app_state);
     assets *Assets = &AppState->Assets;
     menu *MainMenu = &AppState->MainMenu;
     platform_work_queue *Queue = &p->Queue;
     camera *C = &AppState->Camera;
-    
-    qlibCoordSystem(QLIB_CENTER);
     
     if (!AppState->Initialized) {
         AppState->Initialized = true;
@@ -114,18 +112,21 @@ UpdateRender(platform *p)
         C->Up = v3(0, 1, 0);
         C->FOV = 90.0f;
         C->Mode3D = false;
+        
+        SetFalse(&p->AudioState.Paused);
     }
     
     C->PlatformDim = v2(p->Dimension.Width, p->Dimension.Height);
+    
     playing_sound *PlayingSound = &p->AudioState.PlayingSounds[0];
+    loaded_sound *LoadedSound = GetSound(Assets, GetFirstSound(Assets, Asset_Song));
     
     if (DoMenu(MainMenu, "main.menu", p, &AppState->Assets, IDs, MCI_Count))
     {
         if (MainMenu->Events.ButtonClicked == MCI_Play)
         {
+            PlaySound(&p->AudioState, GetFirstSound(Assets, Asset_Song));
             SetTrue(&p->AudioState.Paused);
-            if (PlayingSound->CurrentVolume.x == 0)
-                PlaySound(&p->AudioState, GetFirstSound(Assets, Asset_Song));
         }
         if (MainMenu->Events.ButtonClicked == MCI_Pause)
         {
@@ -133,13 +134,29 @@ UpdateRender(platform *p)
         }
     }
     
+    qlibCoordSystem(QLIB_TOP_RIGHT);
+    
     v2 WindowDim = GetDim(p);
-    v2 Corner = GetTopLeftCornerCoords(p);
+    v2 Corner = v2(0, 0);
     real32 Width = 2.0f;
     real32 JumpWidth = 0.0f;
     
-    if (PlayingSound->CurrentVolume.x != 0) {
-        loaded_sound *LoadedSound = GetSound(Assets, PlayingSound->LoadedSound);
+    if (KeyDown(&p->Input.Mouse.Left)) {
+        v2 MouseCoords = v2(p->Input.Mouse.X, p->Input.Mouse.Y);
+        if (CoordsInRect(MouseCoords, Corner, v2(WindowDim.x, 50.0f))) {
+            
+            real32 NewPercentDone = MouseCoords.x / WindowDim.x;
+            PlayingSound->SamplesPlayed = (int32)(NewPercentDone * LoadedSound->SampleCount);
+            AppState->SaveSample = PlayingSound->SamplesPlayed;
+        }
+    }
+    
+    // Progress Bar
+    real32 PercentDone = (real32)PlayingSound->SamplesPlayed / (real32)LoadedSound->SampleCount;
+    Push(v3(Corner.x, Corner.y, 0), v2(PercentDone * WindowDim.x, 50.f), 0xFFF8CB2E, 0.0f);
+    
+    // Sound Visual
+    if (PlayingSound->Playing) {
         real32 PercentDone = (real32)PlayingSound->SamplesPlayed / (real32)LoadedSound->SampleCount;
         
         for (int i = AppState->SaveSample; i < PlayingSound->SamplesPlayed; i+=1) {
@@ -150,24 +167,20 @@ UpdateRender(platform *p)
             if (LoadedSound->Samples[0][i] > AppState->BiggestSound)
                 AppState->BiggestSound = LoadedSound->Samples[0][i];
             
-            //fprintf(stderr, "%d\n", PlayingSound->LoadedSound->Samples[0][i]);
-            
             if (X + JumpWidth > WindowDim.x) {
                 AppState->SaveSample = i;
                 JumpWidth = 0;
             }
             
             if (Height > 0)
-                Push(v3(Corner.x + X +JumpWidth, 0, 0), v2(Width, Height), 0xFFB22727, 0.0f);
+                Push(v3(Corner.x + X +JumpWidth, WindowDim.y/2, 0), v2(Width, Height), 0xFFB22727, 0.0f);
             else if (Height < 0)
-                Push(v3(Corner.x + X +JumpWidth, Height, 0), v2(Width, -Height), 0xFFB22727, 0.0f);
+                Push(v3(Corner.x + X +JumpWidth, Height + WindowDim.y/2, 0), v2(Width, -Height), 0xFFB22727, 0.0f);
             JumpWidth += Width;
         }
-        
-        Push(v3(Corner.x, Corner.y, 0), v2(PercentDone * WindowDim.x, 50.f), 0xFFF8CB2E, 0.0f);
     }
     
-    DrawFPS(p->Input.MillisecondsElapsed, GetDim(p), GetFont(Assets, GetFirstFont(Assets, Asset_Font)));
+    //DrawFPS(p->Input.MillisecondsElapsed, GetDim(p), GetFont(Assets, GetFirstFont(Assets, Asset_Font)));
     DrawMenu(MainMenu, v2(0, 0), GetDim(p), 100.0f);
     
     RenderPieceGroup(C, Assets);
